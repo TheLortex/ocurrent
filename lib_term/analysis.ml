05bcb368c65119aeed722b7bc8fc56d6be512785
Lucas Pluvinage <lucas@tarides.com>
2021-02-11 12:48:35 +0100

    first proof of concept

--- lib_term/analysis.ml
+++ lib_term/analysis.ml
@@ -44,8 +44,17 @@ module Make (Meta : sig type t end) = struct
     in
     aux f (Term x)
 
-    let pp_meta f = function 
-    | Constant None -> Fmt.pf f "<>" 
+    let rec pp_meta f t = 
+      let Term t = t in
+      match t.ty with 
+    | Constant None -> 
+      begin match t.bind with
+        | Some ctx -> Fmt.pf f "<%a>" pp_meta ctx
+        | None ->
+          match Current_incr.observe t.v with
+          | Error (_, `Active _) -> Fmt.string f "(input)"
+          | _ -> Fmt.string f "(const)"
+      end
     | Constant (Some l) -> Fmt.pf f "<%s>" l
     | Map_input { source = _; info = Ok label } -> Fmt.string f label
     | Map_input { source = _; info = Error `Blocked } -> Fmt.string f "(blocked)"
@@ -53,19 +62,17 @@ module Make (Meta : sig type t end) = struct
     | Opt_input _ -> Fmt.pf f "opt_input"
     | Bind_in _ -> Fmt.pf f "bind_in"
     | Bind_out _ -> Fmt.pf f "bind_out"
-    | Primitive {info; _} -> Fmt.pf f "|>> %s" info
+    | Primitive {info; _} -> Fmt.pf f "|>> %s" (Astring.String.map (function '\n' -> ' ' | c -> c ) info)
     | Pair _ -> Fmt.pf f "pair"
     | Gate_on _ -> Fmt.pf f "gate"
     | List_map _ -> Fmt.pf f "list map" 
     | Option_map _ -> Fmt.pf f "option map" 
     | State _ -> Fmt.pf f "state"
-    | Catch _ -> Fmt.pf f "catch"
-    | Map _ -> Fmt.pf f "mpa"
+    | Catch {label; _} -> Fmt.pf f "catch (%s)" label
+    | Map x -> Fmt.pf f "map (%a)" pp_meta x
     | Collapse _ -> Fmt.pf f "collapse"
 
-  module Node_set = Set.Make(struct type t = int let compare = compare end)
-
-  module Out_node = struct
+  module Out_node_make(Node_set: Set.S) = struct
     (* Information about one node in the OCurrent graph. *)
 
     type t = {
@@ -77,6 +84,9 @@ module Make (Meta : sig type t end) = struct
          `outputs = {A, B}`. Anything that takes input from C will instead take
          input directly from both A and B. *)
 
+      deps : Node_set.t;
+      (* The set of nodes that are direct dependencies. *)
+
       trans : Node_set.t;
       (* The set of nodes which must be resolved for this node to be resolved,
          excluding [outputs]. For example, in the graph `A -> B -> C`:
@@ -87,11 +97,14 @@ module Make (Meta : sig type t end) = struct
 
          This is used to hide edges that are implied by other edges, to simplify
          the output. *)
+      label : string option;
     }
 
     let empty = {
       outputs = Node_set.empty;
       trans = Node_set.empty;
+      deps = Node_set.empty;
+      label = None;
     }
 
     let is_empty t = Node_set.is_empty t.outputs
@@ -116,9 +129,16 @@ module Make (Meta : sig type t end) = struct
       let outputs =
         if Node_set.is_empty outputs then a.outputs
         else outputs
+      in
+      let label = match a.label, b.label with 
+        | None, None -> None
+        | None, Some a | Some a, None -> Some a
+        | Some a, Some b -> Some (a^"/"^b)
       in {
         outputs;
         trans = Node_set.union a.trans b.trans;
+        deps = Node_set.union a.deps b.deps;
+        label
       }
 
     (* Connect [t]'s outputs using [make_edge] to connect to something. *)
@@ -126,119 +146,228 @@ module Make (Meta : sig type t end) = struct
       Node_set.iter make_edge t.outputs
 
     (* An ordinary node, represented by a single box in the diagram. *)
-    let singleton ~deps i = {
+    let singleton ~deps ?label ?(direct_deps=deps) i = {
       outputs = Node_set.singleton i;
       trans = Node_set.union deps.outputs deps.trans;
+      deps = direct_deps.outputs;
+      label;
     }
   end
+
+  module Node_set = Set.Make(struct type t = int let compare = compare end)
+
+  module Out_node = Out_node_make(Node_set)
+
+  module Id_out_node = Out_node_make(Id.Set)
 
   let colour_of_activity = function
     | `Ready -> "#ffff00"
     | `Running -> "#ffa500"
 
-
-
-
-  type dag = Seq of (string * dag list) | Par of dag list | Node of generic | Empty_node
+  type dag = Seq of (string option * dag list) | Par of dag list | Node of generic | Empty_node
   
+  let rec pp_dag f = function 
+    | Empty_node -> Fmt.string f "<>"
+    | Node t -> Fmt.pf f "<%a>" pp_meta t
+    | Par lst -> Fmt.pf f "@[PAR[%a]@]" (Fmt.list ~sep:(fun f () -> Fmt.pf f "|@,") pp_dag) lst
+    | Seq (None, lst) -> Fmt.pf f "@[SEQ(%a)@,@]" (Fmt.list ~sep:(fun f () -> Fmt.pf f ";@,") pp_dag) lst 
+    | Seq (Some info, lst) -> Fmt.pf f "@[SEQ(%s)(%a)@,@]" info (Fmt.list ~sep:(fun f () -> Fmt.pf f ";@,") pp_dag) lst 
 
-  (* Given two nodes, returns the closest ancestor if it exists, or None. *)
-  let get_common_ancestor t1 t2 =
-    let ancestors_1 = ref Id.Map.empty in
-    let ancestors_2 = ref Id.Map.empty 
+  let to_dag t = 
+    let seen : Id_out_node.t Id.Map.t ref = ref Id.Map.empty in
+    let nodes : generic Id.Map.t ref = ref Id.Map.empty 
     in
-    let explore map_ref (Term t) = (* BFS *)
-      let to_do = Queue.create () in 
-      Queue.add (Term t, 0) to_do;
-      while not (Queue.is_empty to_do) do 
-        let (Term t, distance) = Queue.pop to_do in
-        if Id.Map.mem t.id !map_ref then
-          ()
-        else
-          map_ref := Id.Map.add t.id (Term t, distance) !map_ref;
-          match t.ty with 
-          | Primitive {x; _} -> Queue.add (x, distance+1) to_do
-          | Pair (a1, a2) -> (Queue.add (a1, distance+1) to_do; Queue.add (a2, distance+1) to_do)
-          | Gate_on {ctrl; value; _} -> (Queue.add (ctrl, distance+1) to_do; Queue.add (value, distance+1) to_do)
-          | Map x -> Queue.add (x, distance+1) to_do
-          | Bind_out v -> Queue.add (Current_incr.observe v, distance+1) to_do
-          | Catch {source; _} -> Queue.add (source, distance+1) to_do
-          | _ -> ()
-      done
-    in
-    explore ancestors_1 t1;
-    explore ancestors_2 t2;
-    let common = Id.Map.merge 
-    (fun _ d1 d2 -> match (d1,d2) with 
-      | Some (t, d1), Some (_, d2) -> Some (t, d1+d2)
-      | _ -> None) 
-      !ancestors_1 
-      !ancestors_2 
-    in
-    Id.Map.fold 
-    (fun id (t, distance) -> function 
-      | Some (min_id, (_, min_distance)) when distance > min_distance -> Some (min_id, (t, min_distance))
-      | _ -> Some (id, (t, distance)))
-      common
-      None
-    |> Option.map (fun (_, (t, _)) -> t) 
-    
-  let rec to_dag (Term t) stop_at =
-    match stop_at with 
-    | Some (Term t_stop) when t.id = t_stop.id -> (Printf.printf "the end\n%!"; Empty_node)
-    | _ ->
-    match t.ty with 
-    | Primitive {x; info; _} -> (Printf.printf "primitive\n%!"; Seq (info, [to_dag x stop_at; Node (Term t)]))
-    | Pair (a1, a2) -> 
-      begin 
-        Printf.printf "pair\n%!"; 
-        match get_common_ancestor a1 a2 with 
-        | None -> Par ([to_dag a1 stop_at; to_dag a2 stop_at])
-        | Some node -> Seq ("pair", [to_dag node stop_at; Par ([to_dag a1 (Some node); to_dag a2 (Some node)])])
-      end
-    | Gate_on {ctrl; value; _} ->
-      begin
-        Printf.printf "gate_on\n%!"; 
-        match get_common_ancestor ctrl value with 
-        | None -> Par ([to_dag ctrl stop_at; to_dag value stop_at])
-        | Some node -> Seq ("gate", [to_dag node stop_at; Par ([to_dag ctrl (Some node); to_dag value (Some node)])])
-      end
-    | Map next -> to_dag next stop_at
-    | Bind_in (value, _) -> to_dag value stop_at
-    | Bind_out value ->  to_dag (Current_incr.observe value) stop_at
-    | Map_input _ -> (Printf.printf "Map_input\n%!"; Node (Term t))
-    | Opt_input _ -> (Printf.printf "Opt_input\n%!"; Node (Term t))
-    | State _ -> (Printf.printf "State\n%!"; Node (Term t))
-    | Catch {source; label; _} -> (match t.bind with 
-      | None -> Seq (label, [to_dag source stop_at])
-      | Some ctx -> Seq (label, [to_dag ctx stop_at; to_dag source stop_at]))
-    
-    | List_map _ -> (Printf.printf "List_map\n%!"; Node (Term t))
-    | Option_map _ -> (Printf.printf "Option_map\n%!"; Node (Term t))
-    | Collapse _ -> (Printf.printf "Collapse\n%!"; Node (Term t))
-    | Constant (Some _) -> (match t.bind with 
-      | None -> Node (Term t)
-      | Some ctx -> Seq ("cst", [to_dag ctx stop_at; Node (Term t)]))
-    | Constant None -> (match t.bind with 
-      | None -> Node (Term t)
-      | Some ctx -> to_dag ctx stop_at)
+    let rec aux (Term t) =
+      match Id.Map.find_opt t.id !seen with
+      | Some x -> x
+      | None ->
+        let ctx =
+          match t.bind with
+          | None -> Id_out_node.empty
+          | Some c -> aux c
+        in
+        let v = Current_incr.observe t.v in
+        let error_from_self =
+          match v with
+          | Ok _ -> false
+          | Error (id, _) when Id.equal id t.id -> true
+          | Error (id, _) ->
+            match t.ty with
+            | Collapse { input = Term input; _} ->
+              (* The error isn't from us. but this is a collapsed node. If we're just propagating
+                 an error from our input then keep that, but report errors from within the collapsed
+                 group as from us. *)
+              begin match Current_incr.observe input.v with
+                | Error (orig_id, _) -> not (Id.equal id orig_id)
+                | Ok _ -> true
+              end
+            | _ -> false
+        in
+        let outputs =
+          match t.ty with
+          | Constant (Some l) -> Id_out_node.singleton ~deps:ctx t.id
+          | Constant None when Id_out_node.is_empty ctx ->
+            if Result.is_ok v then ctx
+            else (
+              Id_out_node.singleton ~deps:ctx t.id
+            )
+          | Constant None -> ctx
+          | Map_input { source; info } ->
+            let label =
+              match info with
+              | Ok l -> l
+              | Error `Blocked -> "(each item)"
+              | Error `Empty_list -> "(empty list)"
+            in
+            let source = aux source in
+            let deps = Id_out_node.union source ctx in
+            Id_out_node.singleton ~direct_deps:source ~deps t.id
+          | Opt_input { source } ->
+            aux source
+          | Bind_in (x, name) ->
+            let inputs =
+              match t.ty with
+              | Constant None -> Id_out_node.empty
+              | _ -> aux x
+            in
+            let all_inputs = Id_out_node.union inputs ctx in
+            Id_out_node.singleton ~direct_deps:inputs ~deps:all_inputs t.id
+          | Bind_out x -> aux (Current_incr.observe x)
+          | Primitive {x; info; meta} ->
+            let inputs =
+              match x with
+              | Term { ty = Constant None; _ } -> Id_out_node.empty
+              | _ -> aux x
+            in
+            let all_inputs = Id_out_node.union inputs ctx in
+            Id_out_node.singleton ~direct_deps:inputs ~deps:all_inputs t.id
+          | Pair (x, y) ->
+            let inputs = Id_out_node.union (aux x) (aux y) in
+            let all_inputs = Id_out_node.union ctx inputs in
+            Id_out_node.singleton ~direct_deps:inputs ~deps:all_inputs t.id
+          | Gate_on { ctrl; value } ->
+            let ctrls = aux ctrl in
+            let values = aux value in
+            let data_inputs = Id_out_node.union values ctx in
+            let deps = Id_out_node.(union ctrls data_inputs) in
+            Id_out_node.singleton ~direct_deps:(Id_out_node.union ctrls values) ~deps t.id
+          | State { source } ->
+            let inputs = aux source in
+            (* Because a state node will be ready even when its inputs aren't, we shouldn't
+               remove dependencies just because they're also dependencies of a state node.
+               e.g. setting a GitHub status depends on knowing which commit is to be tested
+               and the state of the build. We can know the state of the build (pending) without
+               yet knowing the commit. So the set_state node can't run even though its
+               state input is ready and transitively depends on knowing the commit. *)
+            Id_out_node.singleton ~direct_deps:inputs ~deps:Id_out_node.empty t.id
+          | Catch { source; label; _ } ->
+            let inputs = aux source in
+            let all_inputs = Id_out_node.union inputs ctx in
+            Id_out_node.singleton ~label ~direct_deps:inputs ~deps:all_inputs t.id
+          | Map x ->
+            let inputs = aux x in
+            begin match v with
+              | Error (_, `Msg _) when error_from_self ->
+                (* Normally, we don't show separate boxes for map functions.
+                   But we do if one fails. *)
+                let all_inputs = Id_out_node.union inputs ctx in
+                Id_out_node.singleton ~direct_deps:inputs ~deps:all_inputs t.id
+              | _ ->
+                aux x
+            end
+          | List_map { items; output } ->
+            ignore (aux items);
+            let outputs = aux (Current_incr.observe output) in
+            outputs
+          | Option_map { item; output } ->
+            ignore (aux item);
+            aux (Current_incr.observe output)
+          | Collapse { key; value; input; output } ->
+            let inputs = aux input in
+            let all_inputs = Id_out_node.union inputs ctx in
+            let id = Id.mint () in
+            let fake_node = Id_out_node.singleton ~direct_deps:inputs ~deps:all_inputs id in
+            seen := Id.Map.add id fake_node !seen;
+            let outputs = aux output in
+            let all_outputs = Id_out_node.union outputs ctx in
+            let id = Id.mint () in
+            let good_node = Id_out_node.singleton ~label:key ~direct_deps:outputs ~deps:all_outputs id in
+            seen := Id.Map.add id good_node !seen;
+            Id_out_node.singleton ~direct_deps:(Id_out_node.union fake_node good_node) ~deps:(Id_out_node.union fake_node good_node) t.id
+        in
+        seen := Id.Map.add t.id outputs !seen;
+        nodes := Id.Map.add t.id (Term t) !nodes;
+        outputs 
+      in
+     let _ = aux t in
+    !seen, !nodes
 
+
+  let reduce f = List.fold_left (fun a b -> match a with 
+    | None -> Some b
+    | Some a -> Some (f a b)) None
+
+
+  let get_topological_bottoms nodes deps =
+    (* given a set of nodes, find the one that don't have any dependencies *)
+    let res = ref deps in
+    let remove_parents dep = 
+      let (node: Id_out_node.t) = Id.Map.find dep nodes in
+      res := Id.Set.diff !res node.deps
+    in
+    Id.Set.iter remove_parents deps;
+    !res
+
+  let to_dag (Term t) =
+    let nodes, term_reverse_map = to_dag (Term t) in
+    let rec explore (ids: Id.t list) (stop_at: Id.Set.t) =
+      let perform_single id stop_at =  
+        if Id.Set.mem id stop_at then Empty_node
+        else 
+          let node = Id.Map.find id nodes in
+          let parents = node.deps in
+          match Id.Map.find_opt id term_reverse_map, Id.Set.to_seq parents |> List.of_seq with 
+          | Some term, [] -> Node term
+          | Some term, req -> Seq (node.label, [explore req stop_at; Node term])
+          | None, [] -> Empty_node
+          | None, req -> Seq (node.label, [explore req stop_at])
+      in
+      (* explore that list of nodes *)
+      match ids with 
+      | [] -> Empty_node
+      | [id] -> perform_single id stop_at
+      | lst -> 
+        let transdeps = lst |> List.map (fun id -> (Id.Map.find id nodes).trans) |> reduce Id.Set.inter |> Option.get in
+        let op = Par (lst |> List.map (fun id -> perform_single id (transdeps |> Id.Set.union stop_at))) in
+        let rest = get_topological_bottoms nodes transdeps |> Id.Set.to_seq |> List.of_seq in
+        Seq (None, [explore rest stop_at; op] )
+    in
+    explore [t.id] Id.Set.empty
 
   let remove_type =
     function 
-  | Constant None -> true
+  | Constant None
+  | Collapse _
+  | Gate_on _
+  | Bind_in _
+  | Bind_out _
+  | Catch _
+  | Pair (_,_) -> true
   | _ -> false
 
   let rec simplify = function 
   | Node (Term t) when remove_type t.ty -> None
   | Empty_node -> None
-  | Seq (lbl, lst) -> 
+  | Seq (Some lbl, lst) -> 
+    let lst = List.filter_map simplify lst in
+    Some (Seq (Some lbl, lst))
+  | Seq (None, lst) -> 
     let lst = List.filter_map simplify lst in
     (match lst with 
     | [] -> None
-    | [Seq (_, lst)] -> Some (Seq (lbl, lst))
+    | [Seq (lbl, lst)] ->  Some (Seq (lbl, lst))
     | [v] -> Some v
-    | lst -> Some (Seq (lbl, lst)))
+    | lst -> Some (Seq (None, lst)))
   | Par lst ->
     let lst = List.filter_map simplify lst in
     (match lst with 
@@ -251,7 +380,7 @@ module Make (Meta : sig type t end) = struct
   | Seq (lbl, lst) ->
     let lst = List.map 
       (fun node -> match flatten node with 
-        | Seq (_, lst) -> lst 
+        | Seq (None, lst) -> lst 
         | node -> [node])
       lst |> List.flatten in 
     Seq (lbl, lst)
@@ -318,16 +447,27 @@ module Make (Meta : sig type t end) = struct
     | Empty_node -> `Error
 
 
-  let rec pp_dag ~job_info f = function
+  let rec pp_html_dag ~job_info f value = 
+    let status = get_status value in
+    let preopen = match status with 
+      | `Error | `Running | `Ready -> " open"
+      | _ -> "" 
+    in
+    match value with
     | Par lst -> begin 
       Fmt.pf f "<div>"; 
-      List.iter (fun i -> Fmt.pf f "<div style='margin: 10px; padding-left: 16px; border-left: solid %s 3px'><div>%a</div></div>" (get_status i |> color_of_status) (pp_dag ~job_info) i) lst; 
+      List.iter (fun i -> Fmt.pf f "<div style='margin: 4px; padding-left: 16px; border-left: solid %s 3px'><div>%a</div></div>" (get_status i |> color_of_status) (pp_html_dag ~job_info) i) lst; 
       Fmt.pf f "</div>"
     end
-    | Seq (lbl, lst) -> begin 
-      Fmt.pf f "<details><summary>%s</summary><ol style='list-style: none;'>" lbl; 
-      List.iteri (fun k i -> Fmt.pf f "<li><span style='color: %s'>%d) </span><div style='display: inline-block; vertical-align: top'>%a</div>" (get_status i |> color_of_status) (k+1) (pp_dag ~job_info) i) lst; 
+    | Seq (Some lbl, lst) -> begin 
+      Fmt.pf f "<details%s><summary>%s</summary><ol style='list-style: none; padding-left: 0'>" preopen lbl; 
+      List.iteri (fun k i -> Fmt.pf f "<li><span style='color: %s'>%d. </span><div style='display: inline-block; vertical-align: top'>%a</div>" (get_status i |> color_of_status) (k+1) (pp_html_dag ~job_info) i) lst; 
       Fmt.pf f "</ol></details>"
+    end
+    | Seq (None, lst) -> begin
+      Fmt.pf f "<ol style='list-style: none; padding-left: 0'>";
+      List.iteri (fun k i -> Fmt.pf f "<li><span style='color: %s'>%d. </span><div style='display: inline-block; vertical-align: top'>%a</div>" (get_status i |> color_of_status) (k+1) (pp_html_dag ~job_info) i) lst;
+      Fmt.pf f "</ol>";
     end
     | Node (Term t) -> (match t.ty with 
       | Primitive {info; meta; _} ->
@@ -337,15 +477,14 @@ module Make (Meta : sig type t end) = struct
           | Some id -> job_info id
         in
          Fmt.pf f "<a href='%a'>%s</a>" (Fmt.option Fmt.string) url info
-      | Constant (Some str) -> Fmt.pf f ">%s" str
-      | Constant None -> Fmt.pf f "><" 
-      | meta -> Fmt.pf f "%a" pp_meta meta
+      | meta -> Fmt.pf f "%a" pp_meta (Term t)
     )
     | Empty_node -> ()
 
   let pp_html ~job_info f x = 
-    let dag = to_dag (Term x) None |> simplify |> Option.get |> flatten in
-    pp_dag ~job_info f dag
+    let dag = to_dag (Term x) in
+    let dag = dag |> simplify |> Option.get |> flatten in
+    pp_html_dag ~job_info f dag
 
   let pp_dot ~env ~collapse_link ~job_info f x =
     let env = Env.of_seq (List.to_seq env) in
